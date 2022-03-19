706b6c5e34024efa2e6942e50ae3428e866e4bd3
RAJDAOUI Zouhir <zouhir.rajdaoui@inyad.com>
2022-03-19 04:45:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Portfolio as Single Page Application
+
+## The idea behind the app
+
+## Technologies
+
+## link to live
+
+## What's next?
---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '../src/Link';
+import Copyright from '../src/Copyright';
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          My portfolio in NEXT Js
+        </Typography>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
